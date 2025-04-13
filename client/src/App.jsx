95af4f140305d25883c30382fc8e9cf49545abb3
Kautilya Veer <kautilyaveer@eduroam-226-99-201.laptops.usf.edu>
2025-04-13 07:32:04 -0400

    refinement 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,15 +12,14 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home" element={<LandingPage />}/>
+        <Route path="/" element={<Navigate to="/login" />} />
+        {/* <Route path="/home" element={<LandingPage />}/> */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/health" element={<HealthForm />} />
         <Route path="/history" element={<HealthChart />} />
         <Route path="/predict" element={<StrokePredictChart />} />
-        <Route path="/meals" element={<MealSuggestion />} />
       </Routes>
     </BrowserRouter>
   );
